609623a934abe3017e4953b47f79ad7efc586bb9
sujoy743175 <sujoy743175@yahoo.co.in>
2021-07-22 19:55:22 +0530

    modified main.py now it's independant

--- app/start.py
+++ app/start.py
@@ -1,3 +1,11 @@
+from app.obstacleavoidance import avoid
+from app.connect_and_update import connectToWifiAndUpdate
+from app.blink import blink
+
 def start():
-    print('now its working..') 
+    print('now its working..')
+    connectToWifiAndUpdate()
+    blink()
+    #avoid()   
+
 
--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-from machine import Pin, PWM
-
-import time
-
-from app.obstacleavoidance import avoid
-from app.connect_and_update import connectToWifiAndUpdate
-from app.start import start
-from app.blink import blink
-
-connectToWifiAndUpdate()
+from app.start import *
 
 while True:
     start()
-    blink()
-    #avoid()
-
