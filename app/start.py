2674d64875a5cc2b241dd7d9c4ca602d24c1b55a
sujoy743175 <sujoy743175@yahoo.co.in>
2021-07-22 20:08:27 +0530

    update main.py

--- app/start.py
+++ app/start.py
@@ -1,11 +1,9 @@
 from app.obstacleavoidance import avoid
-from app.connect_and_update import connectToWifiAndUpdate
 from app.blink import blink
 
 def start():
     print('now its working..')
-    connectToWifiAndUpdate()
     blink()
-    #avoid()   
+    #avoid()    
 
 
