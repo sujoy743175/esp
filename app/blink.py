23234b84eb268abcb3227c48a91ef3fde89792eb
sujoy743175 <sujoy743175@yahoo.co.in>
2021-07-23 19:59:07 +0530

    blink.py amended

--- app/blink.py
+++ app/blink.py
@@ -3,12 +3,19 @@ import time
 
 led_builtin = Pin(2, Pin.OUT)
 
+
+
 def blink():
-    led_builtin.value(1)
-    print('led on ...!')
-    time.sleep(1)
-    led_builtin.value(0)
-    print('led off ...!')
-    time.sleep(1)
+    while True:
+        led_builtin.value(not led_builtin.value())
+        time.sleep_ms(1000)
+        
+    # led_builtin.value(1)
+    # print('led on ...!')
+    # time.sleep(1)
+    # led_builtin.value(0)
+    # print('led off ...!')
+    # time.sleep(1)
     
     
+
